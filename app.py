931c2dec7abf9f3f2155d70091fca3f9fe078ac9
ViralModi228 <modi.viral228@gmail.com>
2024-10-24 00:56:21 +0530

    third commit

--- app.py
+++ app.py
@@ -5,48 +5,51 @@ from nltk.corpus import stopwords
 import nltk
 from nltk.stem.porter import PorterStemmer
 
-ps=PorterStemmer()
+# Download necessary NLTK data
+nltk.download('punkt')
+nltk.download('stopwords')
 
+ps = PorterStemmer()
+
+# Load your model and vectorizer
 tfidf = pickle.load(open('vectorizer.pkl','rb'))
 model = pickle.load(open('model.pkl','rb'))
 
 st.title("Email Classifier")
 
 def transform_text(text):
-  text = text.lower() # lower case
-  text = nltk.word_tokenize(text) # tokenization i.e list of words
-  y = []
-  for i in text:
-    if i.isalnum(): # removing special characters
-      y.append(i)
+    text = text.lower()  # Convert to lowercase
+    text = nltk.word_tokenize(text)  # Tokenize
+    y = []
+    for i in text:
+        if i.isalnum():  # Keep only alphanumeric tokens
+            y.append(i)
+    text = y[:]
+    y.clear()
 
-  text = y[:]
-  y.clear()
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+    
+    text = y[:]
+    y.clear()
 
-  for i in text:
-    if i not in stopwords.words('english') and i not in string.punctuation:
-      y.append(i)
-
-  text = y[:]
-  y.clear()
-
-  for i in text:
-    y.append(ps.stem(i))
-
-  return " ".join(y)
+    for i in text:
+        y.append(ps.stem(i))  # Apply stemming
+    
+    return " ".join(y)
 
 input_msg = st.text_input("Enter the Email/Message")
 
 if st.button('Predict'):
-    # 1.preprocess
+    # 1. Preprocess the input
     new_text = transform_text(input_msg)
-    # 2.vectorize
+    # 2. Vectorize the input
     vector_input = tfidf.transform([new_text])
-    # 3.predict
+    # 3. Make prediction
     result = model.predict(vector_input)[0]
-    # 4.display
+    # 4. Display result
     if result == 1:
         st.header('SPAM')
     else:
         st.header('NOT SPAM')
-
